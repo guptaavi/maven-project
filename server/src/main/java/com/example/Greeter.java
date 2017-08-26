42cd89c638eea7f307b7f1ea67421d08b5357070
Avinash Gupta <agupta30@gmail.com>
2017-08-25 20:56:56 -0700

    Checkstyle fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,10 @@ public class Greeter {
 
   }
 
-  /**
-   * This is a method.
-   */
+    /**
+     * @param someone
+     * @return
+     */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
